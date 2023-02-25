367d52b210abab2ac817e9642ed4b740059e6fd6
smdtk <kim_tae_ho@hotmail.com>
2023-02-24 23:20:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+DFRobotMaqueenPlusV2.init()
+basic.showIcon(IconNames.Yes)
+let P = true
+basic.forever(function () {
+    if (input.soundLevel() > 100) {
+        P = !(P)
+        if (P == true) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 100)
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eOpen)
+        } else {
+            DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+            DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
+        }
+    }
+})
